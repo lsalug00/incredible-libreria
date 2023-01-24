5c6216b85b1ad6bd0fa8d46172bfbcc6e1a49cd2
David <davidrojomar12@gamil.com>
2023-01-24 12:42:49 +0100

    Segundo commit

--- Libreria.java
+++ Libreria.java
@@ -6,4 +6,20 @@ public class Libreria{
         this.nombre = nombre;
         this.direccion = direccion;
     }
+    
+    public void setNombre (String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setDireccion (String direccion) {
+        this.direccion = direccion;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getDireccion () {
+        return direccion;
+    }
 }
