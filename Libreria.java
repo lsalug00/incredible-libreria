2895f22a591f55f2b7867192ade48804ec67e499
lsalug00 <lidiasaludes@gmail.com>
2023-01-24 12:34:23 +0100

    Primer commit

---
+++ Libreria.java
@@ -0,0 +1,9 @@
+public class Libreria{
+    private String nombre;
+    private String direccion;
+    
+    public Libreria(String nombre, String direccion){
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+}
