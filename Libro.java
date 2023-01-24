5c9fc33d1d977650dc64d349f017ed10aaa6017a
lsalug00 <lidiasaludes@gmail.com>
2023-01-24 13:56:52 +0100

    Quinto commit

--- Libro.java
+++ Libro.java
@@ -3,4 +3,10 @@ public class Libro
     private String titulo;
     private String autor;
     private Libreria libreria;
+    
+    public Libro(String titulo, String autor, Libreria libreria){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.libreria = libreria;
+    }
 }
